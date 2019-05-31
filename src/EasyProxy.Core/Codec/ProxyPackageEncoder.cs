905c32a96c7f89b21de2d288db916878ddf728e3
钟泽鹏 <zzp1card1@qq.com>
2019-05-31 18:37:36 +0800

    push

---
+++ src/EasyProxy.Core/Codec/ProxyPackageEncoder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EasyProxy.Core.Codec
+{
+    public class ProxyPackageEncoder : IPackageEncoder<ProxyPackage>
+    {
+        public ReadOnlyMemory<byte> Encode(ProxyPackage package)
+        {
+            var total = ProxyPackage.HEADER_SIZE + ProxyPackage.TYPE_SIZE + ProxyPackage.CONNECTIONID_SIZE + package.Data.Length;
+            var bytes = new byte[total];
+            throw new NotImplementedException();
+        }
+    }
+}
